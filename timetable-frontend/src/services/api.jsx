f17538032cc2e5cf2c5e74b3ca3c88ab8a5d8e6b
Sumit Maurya <hsumit263@gmail.com>
2026-03-30 23:01:37 +0530

    base url modified-II

--- timetable-frontend/src/services/api.jsx
+++ timetable-frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://timetable-app-h2pg.onrender.com/"
+  baseURL: "https://timetable-app-h2pg.onrender.com/api"
 });
 
 // attach token automatically
